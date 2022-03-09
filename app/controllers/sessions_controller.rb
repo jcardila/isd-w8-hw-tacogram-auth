e5a801ec93c47aba1e8dcbfcbdb52a968135d85a
jcardila <andrea.rueda@kellogg.northwestern.edu>
2022-03-09 02:43:37 +0000

    authenticate user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,19 @@ class SessionsController < ApplicationController
   
   def create
     # TODO: authenticate user
+    @user = User.where({ email: params["email"] })[0]
+    if @user
+      if BCrypt::Password.new(@user.password) == params["password"]
+        session["user_id"] = @user.id
+        flash[:notice] = "You logged in!"
+        redirect_to "/posts"
+      else
+          flash[:notice] = "Error logging in!"
+          redirect_to "/sessions/new"
+      end
+    else
+      flash[:notice] = "Error logging in!"
+      redirect_to "/sessions/new"
+    end
   end
 end
