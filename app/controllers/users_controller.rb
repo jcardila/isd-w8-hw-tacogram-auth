0d4edc21552002f70bc69a47735a6c4ba12d748d
jcardila <andrea.rueda@kellogg.northwestern.edu>
2022-03-09 02:36:20 +0000

    ocultar visualmente password y validar no crear cuenta con mismo email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,13 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params["user"])
-    @user.save
+    @user.password = BCrypt::Password.create(params["user"]["password"])
+    if User.where({ email: params["user"]["email"] }).count > 0
+      flash[:notice] = "Email already registered, use a different email address."
+    else
+      @user.save
+    end
+    
     redirect_to "/posts"
   end
 end
