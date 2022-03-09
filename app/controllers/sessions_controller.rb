fcd15dd4b1eda92e5a6278604963fd74af159853
jcardila <andrea.rueda@kellogg.northwestern.edu>
2022-03-09 02:56:02 +0000

    dynamic logged navbar buttons and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,4 +19,11 @@ class SessionsController < ApplicationController
       redirect_to "/sessions/new"
     end
   end
+
+  def destroy
+    session["user_id"] = nil
+    flash[:notice] = "Goodbye."
+    redirect_to "/sessions/new"
+  end
+
 end
